a0e3a1f653682ce30a5c3de853e55c5d87a4caa0
Derrick Galindo <derrickg@tamu.edu>
2020-09-21 15:19:30 -0500

    created user table in database

---
+++ awics_mpt/db/migrate/20200921184301_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :position_id
+      t.integer :member_points
+      t.string :username
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
