064201201e4a719e617b08026163c570600f4d9c
Hard Patel <hard.patel@publicissapient.com>
2020-09-21 21:09:29 -0500

    Added users_controller

--- awics_mpt/config/routes.rb
+++ awics_mpt/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'users#index'
+
+  resources :users do
+    member do
+      get :delete
+    end
+  end
+  #get 'users/show'
+  #get 'users/new'
+  #get 'users/edit'
+  #get 'users/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
