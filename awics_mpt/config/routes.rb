e7ef4eff577b9857db4aaa9bd8c48aa357c94e77
phillipwong95 <phillipwong95@gmail.com>
2020-09-30 12:48:55 -0500

    Added email column to users database and updated htmls to show emails

--- awics_mpt/config/routes.rb
+++ awics_mpt/config/routes.rb
@@ -6,18 +6,12 @@ Rails.application.routes.draw do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
     get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
   end
-  
-  # devise_for :admins
-  # root 'users#index'
 
   resources :users do
     member do
       get :delete
     end
   end
-  #get 'users/show'
-  #get 'users/new'
-  #get 'users/edit'
-  #get 'users/delete'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
