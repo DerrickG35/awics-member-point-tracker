20a634d6684874ffa2a5b52860746fa5c4e1c13d
Hard Patel <hard.patel@publicissapient.com>
2020-10-07 17:04:30 -0500

    Removed Update Points link

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,20 +2,20 @@ class Admin < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    
-    userExists = false
-    @users = User.all
-    @users.each do |user|
-      if user.email == email
-        userExists = true
-        break
-      end
-      if userExists == false
-        return nil
-      end
-    end
 
-    return nil unless email =~ /@tamu.edu\z/ 
+    #userExists = false
+    #@users = User.all
+    #@users.each do |user|
+    #  if user.email == email
+    #    userExists = true
+    #    break
+    #  end
+    #  if userExists == false
+    #    return nil
+    #  end
+    #end
+
+    return nil unless email =~ /@tamu.edu\z/
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 
