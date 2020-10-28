77ba9e90acc9a3e9ecafa2178164fa0b913a958f
Derrick Galindo <derrickg@tamu.edu>
2020-10-28 16:46:42 -0500

    sprint 3 code with new db schema

--- db/migrate/20200930172121_add_emails_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# class for adding emails to respective user accounts
-class AddEmailsToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :email, :string
-  end
-end
--- db/migrate/20201005202741_change_column_name_in_attendance.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# class for tracking the attendance and points of users
-class ChangeColumnNameInAttendance < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :attendances, :user_id, :username
-  end
-end
