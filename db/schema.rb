64f7193ab8140f4a9970728465d227303659b0ef
phillipwong95 <phillipwong95@gmail.com>
2020-11-06 00:14:15 -0600

    added app usage info and removed add new member functionality

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_202741) do
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
-
     t.datetime "event_date"
     t.float "event_points"
     t.text "question"
@@ -44,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_202741) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_events_on_event_id"
-
   end
 
   create_table "positions", force: :cascade do |t|
@@ -56,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_202741) do
   end
 
   create_table "users", force: :cascade do |t|
-
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -72,7 +69,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_202741) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
